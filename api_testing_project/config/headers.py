3e5ef8841a8420e4618ee70a49677cf2c8a705f0
Nick Fedorenko <nfm12121994@gmail.com>
2025-09-26 13:01:11 +0300

    Delete api_testing_project directory

--- api_testing_project/config/headers.py
+++
@@ -1,3 +0,0 @@
-class Head:
-
-    header = {"Content-Type": "application/json"}
--- api_testing_project/services/customer/payloads/payloads.py
+++
@@ -1,6 +0,0 @@
-class Payloads:
-    """Отправляемые данные"""
-
-    debtor_account_vodokomfort = '0014403847'
-    inn_vodokomfort = '7705238125'
-    debtor_account_for_get_customer_by_number = '14480074'
